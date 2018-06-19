728b0613f7abddd84657b93af55651480c2d56d3
Natenael Kelkay <natikelkay@gmail.com>
2018-06-19 15:34:13 -0400

    added maze patterns

--- game/main.py
+++ game/main.py
@@ -18,3 +18,39 @@ class pen(turtle.Turtle):
         turtle.Turtle.__init__(self)
         self.shape("square")
         self.color("white")
+        self.penup()
+        self.speed(0)
+
+
+# Create levels list
+levels = [""]
+
+
+# Define first level
+level_1 = [
+        "XXXXXXXXXXXXXXXXXXXXXXXXX"
+        "X  XXXXXXX          XXXXX"
+        "X  XXXXXXX  XXXXXX  XXXXX"
+        "X       XX  XXXXXX  XXXXX"
+        "X       XX  XXX        XX"
+        "XXXXXX  XX  XXX        XX"
+        "XXXXXX  XX  XXXXXX  XXXXX"
+        "XXXXXX  XX    XXXX  XXXXX"
+        "X  XXX        XXXX  XXXXX"
+        "X  XXX  XXXXXXXXXXXXXXXXX"
+        "X         XXXXXXXXXXXXXXX"
+        "X                XXXXXXXX"
+        "XXXXXXXXXXXX     XXXXX  X"
+        "XXXXXXXXXXXXXXX  XXXXX  X"
+        "XXX  XXXXXXXXXX         X"
+        "XXX                     X"
+        "XXX         XXXXXXXXXXXXX"
+        "XXXXXXXXXX  XXXXXXXXXXXXX"
+        "XXXXXXXXXX              X"
+        "XX   XXXXX              X"
+        "XX   XXXXXXXXXXXXX  XXXXX"
+        "XX    XXXXXXXXXXXX  XXXXX"
+        "XX           XXXX       X"
+        "XXXX                    X"
+        "XXXXXXXXXXXXXXXXXXXXXXXXX"
+        ]
