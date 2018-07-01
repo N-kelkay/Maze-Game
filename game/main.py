ac702605b69c2a4c10bd8c45fa77c3b03e96571e
Natenael Kelkay <natikelkay@gmail.com>
2018-06-30 20:30:13 -0400

    added a treasure class and added it to the game

--- game/main.py
+++ game/main.py
@@ -26,6 +26,22 @@ class Player(turtle.Turtle):
         self.color("blue")
         self.penup()
         self.speed(0)
+        self.gold = 0
+
+
+class Treasure(turtle.Turtle):
+    def __init__(self, x, y):
+        turtle.Turtle.__init__(self)
+        self.shape("circle")
+        self.color("gold")
+        self.penup()
+        self.speed(0)
+        self.gold = 100
+        self.goto(x, y)
+
+    def destroy(self):
+        self.goto(2000, 2000)
+        self.hideturtle()
 
     def go_up(self):
         # self.goto(self.xcor,self.ycor+16)
@@ -65,79 +81,81 @@ levels = [""]
 
 # Define first level
 level_1 = [
-"XXXXXXXXXXXXXXXXXXXXXXXXX",
-"XXXX XXXXXX       XX    X",
-"XXX   XXXX       XXX    X",
-"XX  P  XX        XXX    X",
-"XXX     X    XXXXXXX    X",
-"XXX         XXXXXXX     X",
-"XXXX    X    XXXX       X",
-"XXX    XX     XXX    XXXX",
-"XX    XXX     XXX     XXX",
-"X     XXX     XXXXX     X",
-"XX    XXXXX   XXXXXX    X",
-"XXX  XXXX     XXX       X",
-"X   XXXXX    XXXX       X",
-"X    XXXXXXXXXXXXXXX    X",
-"XX      XXXXXXXXXXXX    X",
-"XXX              XXXX   X",
-"XXXX             XXXX   X",
-"XXXXXXXXXXXXX    XXX    X",
-"XXXXXXXXXXXXX    XXX    X",
-"XXX       XXX    XXX    X",
-"X                XXX    X",
-"XXXX       XXXXXXXX     X",
-"XXXXXX   XXXXXXXX      XX",
-"X                     XXX",
-"X                   XXXXX",
-"XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XXXX XXXXXX       XX    X",
+    "XXX   XXXX       XXX    X",
+    "XX  P  XX        XXX    X",
+    "XXX     X    XXXXXXX    X",
+    "XXX         XXXXXXX     X",
+    "XXXX    X    XXXX      TX",
+    "XXX    XX     XXX    XXXX",
+    "XX    XXX     XXX     XXX",
+    "X     XXX     XXXXX     X",
+    "XX    XXXXX   XXXXXX    X",
+    "XXX  XXXX     XXX       X",
+    "X   XXXXX    XXXX       X",
+    "X    XXXXXXXXXXXXXXX    X",
+    "XX      XXXXXXXXXXXX    X",
+    "XXX              XXXX   X",
+    "XXXX             XXXX   X",
+    "XXXXXXXXXXXXX    XXX    X",
+    "XXXXXXXXXXXXX    XXX    X",
+    "XXX       XXX    XXX    X",
+    "X                XXX    X",
+    "XXXX       XXXXXXXX     X",
+    "XXXXXX   XXXXXXXX      XX",
+    "X                     XXX",
+    "X                   XXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
 ]
 level_2=[
-"XXXXXXXXXXXXXXXXXXXXXXXX",
-"XP XX          XXXXXXXXX",
-"X  XX          XXXXXXXXX",
-"X      XXXXXX  XXXXXXXXX",
-"XXX  XXXXXXXX  XXXXXXXXX",
-"XX   XXXX      XXXXXXXXX",
-"X    XXXX      XXXXXXXXX",
-"X  XXXXXXXXXXXXXXXXXXXXX",
-"X  X           XXXXXXXXX",
-"X  XXXXX XXXX  XXXXXXXXX",
-"X        X  X  XXXXXXXXX",
-"XXXXXXX  X     XXXXXXXXX",
-"X        XXXXXXXXXXXXXXX",
-"X  XX          XXXXXXXXX",
-"X  XXXX        XXXXXXXXX",
-"XXXXXXXXXXXXXXXXXXXXXXXX"
+    "XXXXXXXXXXXXXXXXXXXXXXXX",
+    "XP XX          XXXXXXXXX",
+    "X  XX          XXXXXXXXX",
+    "X      XXXXXX  XXXXXXXXX",
+    "XXX  XXXXXXXX  XXXXXXXXX",
+    "XX   XXXX      XXXXXXXXX",
+    "X    XXXX      XXXXXXXXX",
+    "X  XXXXXXXXXXXXXXXXXXXXX",
+    "X  X           XXXXXXXXX",
+    "X  XXXXX XXXX  XXXXXXXXX",
+    "X        X  X  XXXXXXXXX",
+    "XXXXXXX  X    TXXXXXXXXX",
+    "X        XXXXXXXXXXXXXXX",
+    "X  XX          XXXXXXXXX",
+    "X  XXXX        XXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXX"
   ]
 level_3 = [
-"XXXXXXXXXXXXXXXXXXXXXXXX",
-"XP  XXXXXXX          XXX",
-"X           XXXXXX  XXXX",
-"X  XXXXXXX          XXXX",
-"X  XXXXXXXXXXXXXXXXXXXXX",
-"X  XXXXXXXXXXXXXXXXXXXXX",
-"X                      X",
-"XXXXXXX  XXXXXXXXXXXXXXX",
-"XXXXXX   XXXXXXXXXXXXXXX",
-"XXXXXX                 X",
-"XXXXXX   XXXXXXXXXXXXX X",
-"XXXXXX   XXXXXXXXXXXXX X",
-"X                      X",
-"XXXXX    XXXXXXXXXX    X",
-"X        XXXXXXXXXXX XXX",
-"XXXXXX   XXXXXXXXXXX XXX",
-"X                      X",
-"XXXXXX   XXXX   XXXXXXXX",
-"XXXXXX   XXXX   XXXXXXXX",
-"XXXXX    XXXX          X",
-"X               X      X",
-"XXXXXXX   XXXXXXX  XXXXX",
-"XXXXXX    XXXXXXX  XXXXX",
-"XXXXXXXX           XXXXX",
-"XXXXXXXXXXXXXXXXXXXXXXXX"
+    "XXXXXXXXXXXXXXXXXXXXXXXX",
+    "XP  XXXXXXX          XXX",
+    "X           XXXXXX  XXXX",
+    "X  XXXXXXX          XXXX",
+    "X  XXXXXXXXXXXXXXXXXXXXX",
+    "X  XXXXXXXXXXXXXXXXXXXXX",
+    "X                      X",
+    "XXXXXXX  XXXXXXXXXXXXXXX",
+    "XXXXXX   XXXXXXXXXXXXXXX",
+    "XXXXXX                 X",
+    "XXXXXX   XXXXXXXXXXXXX X",
+    "XXXXXX   XXXXXXXXXXXXX X",
+    "X                      X",
+    "XXXXX    XXXXXXXXXX    X",
+    "X        XXXXXXXXXXX XXX",
+    "XXXXXX   XXXXXXXXXXX XXX",
+    "X                      X",
+    "XXXXXX   XXXX   XXXXXXXX",
+    "XXXXXX   XXXX   XXXXXXXX",
+    "XXXXX    XXXX          X",
+    "X               X     TX",
+    "XXXXXXX   XXXXXXX  XXXXX",
+    "XXXXXX    XXXXXXX  XXXXX",
+    "XXXXXXXX           XXXXX",
+    "XXXXXXXXXXXXXXXXXXXXXXXX"
 ]
 
+# Add a treasure list
+treasures = []
 
 levels.append(level_1)
 
@@ -157,6 +175,9 @@ def setup_maze(level):
 
             if character == "P":
                 player.goto(screen_x, screen_y)
+
+            if character == "T":
+                treasures.append(Treasure(screen_x, screen_y))
 
 
 pen = Pen()
